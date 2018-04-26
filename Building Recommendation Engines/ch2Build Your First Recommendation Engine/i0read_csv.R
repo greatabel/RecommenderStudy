8c36d77992325f6db74504387533b7dddb7b5902
greatabel <myreceiver2for2github@gmail.com>
2018-04-26 15:41:36 +0800

    investigate data

--- Building Recommendation Engines/ch2Build Your First Recommendation Engine/i0read_csv.R
+++ Building Recommendation Engines/ch2Build Your First Recommendation Engine/i0read_csv.R
@@ -3,5 +3,20 @@ library(crayon)
 cat(black$bold$bgCyan("读取电影打分"), '\n')
 
 
-MyData <- read.csv(file="../data/movie_rating.csv", header=TRUE, sep=",")
-MyData
+ratings <- read.csv(file="../data/movie_rating.csv", header=TRUE, sep=",")
+ratings
+
+cat(black$bold$bgCyan("头6条记录"), '\n')
+head(ratings)
+
+cat(black$bold$bgCyan("数据维度"), '\n')
+dim(ratings)
+
+cat(black$bold$bgCyan("输入数据的结构"), '\n')
+str(ratings)
+
+'levels(ratings$critic)'; levels(ratings$critic)
+'levels(ratings$title)';  levels(ratings$title)
+'levels(ratings$rating)'; levels(ratings$rating)
+cat(black$bold$bgCyan("sort(unique(ratings$rating), decreasing = F)"), '\n')
+sort(unique(ratings$rating), decreasing = F)
