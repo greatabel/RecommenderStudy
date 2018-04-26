d698a9967c62c6bd682cbd991702f29314c6ad85
greatabel <myreceiver2for2github@gmail.com>
2018-04-26 16:57:26 +0800

    合并SQL记录和相似性

--- Building Recommendation Engines/ch2Build Your First Recommendation Engine/i3predict_unknown_ratings.R
+++ Building Recommendation Engines/ch2Build Your First Recommendation Engine/i3predict_unknown_ratings.R
@@ -10,7 +10,7 @@ movie_ratings = as.data.frame(
     acast(ratings, title~critic, value.var="rating"))
 sim_users = cor(movie_ratings[,1:6],use="complete.obs")
 
-'提取Toby的电影标题'
+cat(black$bold$bgCyan("提取Toby的电影标题"), '\n')
 rating_critic  = setDT(movie_ratings[colnames(movie_ratings)[6]],
                         keep.rownames = TRUE)[]
 
@@ -22,6 +22,14 @@ titles_na_critic = rating_critic$title[is.na(rating_critic$rating)]
 titles_na_critic
 
 
-cat(black$bold$bgCyan("找出所有记录根据上面过滤出的电影名字"), '\n')
+cat(black$bold$bgCyan("SQL 找出所有记录根据上面过滤出的电影名字"), '\n')
 ratings_t =ratings[ratings$title %in% titles_na_critic,]
+ratings_t
+
+cat(black$bold$bgGreen("添加相似性"), '\n')
+x = (setDT(data.frame(sim_users[,6]),keep.rownames = TRUE)[])
+names(x) = c('critic','similarity')
+x
+cat(black$bold$bgGreen("合并SQL记录和相似性"), '\n')
+ratings_t =  merge(x = ratings_t, y = x, by = "critic", all.x = TRUE)
 ratings_t
