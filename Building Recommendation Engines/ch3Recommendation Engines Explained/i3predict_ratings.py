81f0ed4464d6b0393b85e2e9f34d32417f5347f8
greatabel <myreceiver2for2github@gmail.com>
2018-07-12 23:23:21 +0800

    i3predict_ratings.py

--- Building Recommendation Engines/ch3Recommendation Engines Explained/i3predict_ratings.py
+++ Building Recommendation Engines/ch3Recommendation Engines Explained/i3predict_ratings.py
@@ -11,13 +11,32 @@ if __name__ == "__main__":
     pivot_ratings = ratings.pivot_table(columns='critic',
                                         index='title',
                                         values='rating')
-    print('pivot_ratings:', type(pivot_ratings))
+
     print(pivot_ratings.to_string())
     
     sm = pivot_ratings.corr()
     print(sm.to_string())
 
-    print(colored('预测打分=======>', 'red', attrs=['reverse', 'blink']))
-    missing_films = list(pivot_ratings[pivot_ratings['Toby'].isnull()].index)
+    demo = 'Toby'
+    print(colored('1. 找出该用户为打分的电影 =>', 'red', attrs=['reverse', 'blink']))
+    missing_films = list(pivot_ratings[pivot_ratings[demo].isnull()].index)
     print(missing_films)
+    print(colored('2. 找出该用户已打分的平均分=>', 'red', attrs=['reverse', 'blink']))
 
+    mean_score = pivot_ratings[demo].mean()
+    print(mean_score)
+    print(colored('3. 处理原始表，加上相似性列，筛选出未打分 =>', 
+                  'red', attrs=['reverse', 'blink']))
+    remain_films = ratings[ratings['title'].isin(missing_films)]
+
+    remain_films['similarity'] = remain_films['critic'].map(sm[demo].get)
+    remain_films['sim_rating'] = remain_films.similarity * remain_films.rating
+
+    print(remain_films)
+    print(colored('4. 汇总算出电影平均相似值 =>', 
+                  'red', attrs=['reverse', 'blink']))
+    rec = remain_films.groupby('title').apply(lambda s: s.sim_rating.sum() / s.similarity.sum())
+
+    print(rec)
+    rec = rec[rec >= mean_score]
+    print('该用户我推荐：', list(rec.index))
