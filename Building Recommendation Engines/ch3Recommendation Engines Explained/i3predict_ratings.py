72bb45f278392f8813ed73cbc0e264f04907f20e
greatabel <myreceiver2for2github@gmail.com>
2018-07-12 17:44:59 +0800

    i3predict_ratings.py

--- Building Recommendation Engines/ch3Recommendation Engines Explained/i3predict_ratings.py
+++ Building Recommendation Engines/ch3Recommendation Engines Explained/i3predict_ratings.py
@@ -7,16 +7,13 @@ import i0read_csv
 if __name__ == "__main__":
     ratings = pd.read_csv('../data/movie_rating.csv')
     print(ratings)
+
     pivot_ratings = ratings.pivot_table(columns='critic',
                                         index='title',
                                         values='rating')
     print(pivot_ratings.to_string())
-
-    print(colored('相似度计算=======>', 'red', attrs=['reverse', 'blink']))
-    print('Claudia Puig', pivot_ratings['Claudia Puig'])
-    cor_pr = pivot_ratings.corrwith(pivot_ratings['Claudia Puig'])
-    print(cor_pr,'\n')
-
+    
     sm = pivot_ratings.corr()
     print(sm.to_string())
-    print(colored('可以看出: Lisa Rose ~Toby with 0.99 和 Mick LaSalle ~ Toby 0.92', 'red'))
+
+    print(colored('预测打分=======>', 'red', attrs=['reverse', 'blink']))
