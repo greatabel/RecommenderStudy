d3e1946a25611fbf299401aa6d1bb66b730ce357
greatabel <myreceiver2for2github@gmail.com>
2018-07-17 18:16:16 +0800

    i5linear_regression.R

---
+++ Building Recommendation Engines/ch4DataMining in Recommendation Engines/i5linear_regression.R
@@ -0,0 +1,16 @@
+library(crayon)
+library(MASS)
+
+data("Boston")
+
+set.seed(0)
+which_train <- sample(x = c(TRUE, FALSE), size = nrow(Boston),
+                      replace = TRUE, prob = c(0.8, 0.2))
+
+train <- Boston[which_train, ]
+test <- Boston[!which_train, ]
+
+lm.fit =lm(medv~. ,data=train )
+summary(lm.fit)
+
+pred = predict(lm.fit,test[,-14])
