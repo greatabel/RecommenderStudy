7a941ac5455cc0fffc328538f180aae160e7c1fd
greatabel <myreceiver2for2github@gmail.com>
2018-07-17 18:20:13 +0800

    i7KNN.R

---
+++ Building Recommendation Engines/ch4DataMining in Recommendation Engines/i7KNN.R
@@ -0,0 +1,14 @@
+library(crayon)
+
+cat(black$bold$bgCyan("计算KNN"), '\n')
+
+data("iris")
+library(dplyr)
+iris2 = sample_n(iris, 150)
+train = iris2[1:120,]
+test = iris2[121:150,]
+cl = train$Species
+library(caret)
+fit <- knn3(Species~., data=train, k=3)
+predictions <- predict(fit, test[,-5], type="class")
+table(predictions, test$Species)
