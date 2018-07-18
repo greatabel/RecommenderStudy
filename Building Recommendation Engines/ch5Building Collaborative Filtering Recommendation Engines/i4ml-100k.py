801d3a766c8b624389f795b7706049d98563fe2a
greatabel <myreceiver2for2github@gmail.com>
2018-07-18 11:21:35 +0800

    plt show

--- Building Recommendation Engines/ch5Building Collaborative Filtering Recommendation Engines/i4ml-100k.py
+++ Building Recommendation Engines/ch5Building Collaborative Filtering Recommendation Engines/i4ml-100k.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 path = "../data/ml-100k_udata.csv"
 
@@ -8,3 +8,6 @@ print('type(df)=', type(df))
 print('df.head()=', df.head())
 print('df.columns=', df.columns)
 print('df.shape=', df.shape)
+
+plt.hist(df['Rating'])
+plt.show()
