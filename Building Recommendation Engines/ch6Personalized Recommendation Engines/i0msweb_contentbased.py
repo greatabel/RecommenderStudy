cb80e540e24442eb59f0b45b8a75bd105048a44c
greatabel <myreceiver2for2github@gmail.com>
2018-07-19 10:24:33 +0800

    unique users/webid count

--- Building Recommendation Engines/ch6Personalized Recommendation Engines/i0msweb_contentbased.py
+++ Building Recommendation Engines/ch6Personalized Recommendation Engines/i0msweb_contentbased.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import scipy
 import sklearn
+from termcolor import colored
+
 
 path = "../data/anonymous-msweb.test"
 raw_data = pd.read_csv(path,header=None,skiprows=7)
@@ -15,3 +17,11 @@ user_activity = user_activity[['category','value']]
 # Case (C): This is the case for each user, containing its ID
 # Vote (V): This is the vote lines for the case
 print(user_activity.groupby('category').count())
+print(user_activity.head(15))
+
+print(colored('- . -'*5, 'red', attrs=['reverse', 'blink']))
+w_count = len(user_activity.loc[user_activity['category'] == 'V'].value.unique())
+u_count = len(user_activity.loc[user_activity['category'] == 'C'].value.unique())
+
+print('get unique webid count:', w_count)
+print('get unique users count:', u_count)
