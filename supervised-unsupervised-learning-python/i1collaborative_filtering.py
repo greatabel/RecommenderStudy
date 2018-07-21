7d34e0d904b32f4bb03412ade3d7725a650e3dac
greatabel <myreceiver2for2github@gmail.com>
2018-07-21 18:04:52 +0800

    i1collaborative_filtering.py

---
+++ supervised-unsupervised-learning-python/i1collaborative_filtering.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+import numpy as np
+
+from i0compute_scores import pearson_score
+
+def find_similar_users(dataset, user, num_users):
+    if user not in dataset:
+            raise TypeError('Cannot find ' + user + ' in the dataset')
+    scores = np.array([[x, pearson_score(dataset, user,
+                x)] for x in dataset if x != user])
+    scores_sorted = np.argsort(scores[:, 1])[::-1]
+    top_users = scores_sorted[:num_users]
+    return scores[top_users]
+
+
+if __name__=='__main__':
+
+    user = 'David Smith'
+
+    ratings_file = 'ratings.json'
+
+    with open(ratings_file, 'r') as f:
+        data = json.loads(f.read())
+
+    print('\nUsers similar to ' + user + ':\n')
+    similar_users = find_similar_users(data, user, 3)
+    print('User\t\t\tSimilarity score')
+    print('-'*41)
+    for item in similar_users:
+        print(item[0], '\t\t', round(float(item[1]), 2))
