092571787c1b3ecc50d5a8ea016339087205e2fa
greatabel <myreceiver2for2github@gmail.com>
2018-07-21 17:53:36 +0800

    i0compute_scores.py

--- supervised-unsupervised-learning-python/i0compute_scores.py
+++ supervised-unsupervised-learning-python/i0compute_scores.py
@@ -3,10 +3,63 @@ import numpy as np
 
 
 def euclidean_score(dataset, user1, user2):
-    ""
+    if user1 not in dataset:
+        raise TypeError('Cannot find ' + user1 + ' in the dataset')
+
+    if user2 not in dataset:
+        raise TypeError('Cannot find ' + user2 + ' in the dataset')
+
+    common_movies = {}
+
+    for item in dataset[user1]:
+        if item in dataset[user2]:
+            common_movies[item] = 1
+
+    if len(common_movies) == 0:
+        return 0
+
+    squared_diff = []
+
+    for item in dataset[user1]:
+        if item in dataset[user2]:
+            squared_diff.append(np.square(dataset[user1][item] - dataset[user2][item]))
+
+    return 1 / (1 + np.sqrt(np.sum(squared_diff)))
 
 def pearson_score(dataset, user1, user2):
-    ""
+    if user1 not in dataset:
+        raise TypeError('Cannot find ' + user1 + ' in the dataset')
+
+    if user2 not in dataset:
+        raise TypeError('Cannot find ' + user2 + ' in the dataset')
+
+    common_movies = {}
+
+    for item in dataset[user1]:
+        if item in dataset[user2]:
+            common_movies[item] = 1
+
+    num_ratings = len(common_movies)
+
+    if num_ratings == 0:
+        return 0
+
+    user1_sum = np.sum([dataset[user1][item] for item in common_movies])
+    user2_sum = np.sum([dataset[user2][item] for item in common_movies])
+
+    user1_squared_sum = np.sum([np.square(dataset[user1][item]) for item in common_movies])
+    user2_squared_sum = np.sum([np.square(dataset[user2][item]) for item in common_movies])
+
+    sum_of_products = np.sum([dataset[user1][item] * dataset[user2][item] for item in common_movies])
+
+    Sxy = sum_of_products - (user1_sum * user2_sum / num_ratings)
+    Sxx = user1_squared_sum - np.square(user1_sum) / num_ratings
+    Syy = user2_squared_sum - np.square(user2_sum) / num_ratings
+
+    if Sxx * Syy == 0:
+        return 0
+
+    return Sxy / np.sqrt(Sxx * Syy)
 
 if __name__=='__main__':
     ratings_file = 'ratings.json'
