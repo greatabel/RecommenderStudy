6921838c41003dd61d68a4059de00e6350718478
greatabel <myreceiver2for2github@gmail.com>
2018-07-23 16:34:25 +0800

    pearsonR

--- abel_demo/i5recommend_based_correlations.py
+++ abel_demo/i5recommend_based_correlations.py
@@ -19,3 +19,23 @@ average_rating['ratingCount'] = pd.DataFrame(ratings.groupby('ISBN')['bookRating
 h = average_rating.sort_values('ratingCount', ascending=False).head()
 print(h)
 print('得到最多评价的书籍 并不是平均分最高的')
+print(colored('排除没有投过200票的用户 书籍少于100个投票的也排除掉', 
+              'red', attrs=['reverse', 'blink']))
+
+counts1 = ratings['userID'].value_counts()
+ratings = ratings[ratings['userID'].isin(counts1[counts1 >= 200].index)]
+counts = ratings['bookRating'].value_counts()
+ratings = ratings[ratings['bookRating'].isin(counts[counts >= 100].index)]
+# print(ratings.head(5))
+print('把表转化为 2纬矩阵')
+ratings_pivot = ratings.pivot(index='userID', columns='ISBN').bookRating
+print(ratings_pivot.head(), ratings_pivot.shape)
+
+print('我们找出isbn=0316666343的 打分数第二多的 The Lovely Bones: A Novel')
+bones_ratings = ratings_pivot['0316666343']
+similar_to_bones = ratings_pivot.corrwith(bones_ratings)
+corr_bones = pd.DataFrame(similar_to_bones, columns=['pearsonR'])
+corr_bones.dropna(inplace=True)
+corr_summary = corr_bones.join(average_rating['ratingCount'])
+summary_head = corr_summary[corr_summary['ratingCount']>=300].sort_values('pearsonR', ascending=False).head(10)
+print(summary_head)
