957d9296b9ad6155ca1d136fea17133bc044a5a8
greatabel <myreceiver2for2github@gmail.com>
2018-07-24 15:18:46 +0800

    i6KNN_collaborative_filtering.py

--- abel_demo/i6KNN_collaborative_filtering.py
+++ abel_demo/i6KNN_collaborative_filtering.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.neighbors import NearestNeighbors
+from scipy.sparse import csr_matrix
+from sklearn.decomposition import TruncatedSVD
 
 from termcolor import colored
 
@@ -16,4 +19,79 @@ ratings.columns = ['userID', 'ISBN', 'bookRating']
 print('-----------------------------------------')
 
 print(colored('KNN是一个用来基于共同图书评分以发现相似读者间聚类状况的机器学习算法，并且可以基于距离最近的 k 个邻居的平均评分来进行预测', 
-              'red', attrs=['reverse', 'blink']))
+              'green', attrs=['reverse', 'blink']))
+
+combine_book_rating = pd.merge(ratings, books, on='ISBN')
+columns = ['yearOfPublication', 'publisher', 'bookAuthor', 'imageUrlS', 'imageUrlM', 'imageUrlL']
+# axis : {0 or ‘index’, 1 or ‘columns’}, default 0
+combine_book_rating = combine_book_rating.drop(columns, axis=1)
+print(combine_book_rating.head())
+
+combine_book_rating = combine_book_rating.dropna(axis=0, subset=['bookTitle'])
+
+
+book_ratingCount = (combine_book_rating.
+     groupby(by = ['bookTitle'])['bookRating'].
+     count().
+     reset_index().
+     rename(columns = {'bookRating': 'totalRatingCount'})
+     [['bookTitle', 'totalRatingCount']]
+    )
+print(book_ratingCount.head())
+
+rating_with_totalRatingCount = combine_book_rating.merge(book_ratingCount, 
+                                left_on = 'bookTitle', right_on = 'bookTitle', how = 'left')
+print(rating_with_totalRatingCount.head())
+
+print(colored('统计量', 'green', attrs=['reverse', 'blink']))
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+print(book_ratingCount['totalRatingCount'].describe())
+
+print(colored('看看头部的分布', 'green', attrs=['reverse', 'blink']))
+print(book_ratingCount['totalRatingCount'].quantile(np.arange(.9, 1, .01)))
+print('约有1%的书收到了超过50次的评分，由于数据集内记录众多，我们就截取头部1%的图书来建模')
+
+popularity_threshold = 50
+rating_popular_book = rating_with_totalRatingCount.query('totalRatingCount >= @popularity_threshold')
+print(rating_popular_book.head())
+
+print('只保留美国和加拿大的读者')
+combined = rating_popular_book.merge(users, left_on = 'userID', right_on = 'userID', how = 'left')
+
+us_canada_user_rating = combined[combined['Location'].str.contains("usa|canada")]
+us_canada_user_rating=us_canada_user_rating.drop('Age', axis=1)
+print(us_canada_user_rating.head())
+
+if not us_canada_user_rating[us_canada_user_rating.duplicated(['userID', 'bookTitle'])].empty:
+    initial_rows = us_canada_user_rating.shape[0]
+
+    print('Initial dataframe shape {0}'.format(us_canada_user_rating.shape))
+    us_canada_user_rating = us_canada_user_rating.drop_duplicates(['userID', 'bookTitle'])
+    current_rows = us_canada_user_rating.shape[0]
+    print('New dataframe shape {0}'.format(us_canada_user_rating.shape))
+    print('Removed {0} rows'.format(initial_rows - current_rows))
+
+
+print(colored('应用knn', 'red', attrs=['reverse', 'blink']))
+us_canada_user_rating_pivot = us_canada_user_rating.pivot(index = 'bookTitle', 
+    columns = 'userID', values = 'bookRating').fillna(0)
+us_canada_user_rating_matrix = csr_matrix(us_canada_user_rating_pivot.values)
+
+
+
+model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
+model_knn.fit(us_canada_user_rating_matrix)
+NearestNeighbors(algorithm='brute', leaf_size=30, metric='cosine',
+         metric_params=None, n_jobs=1, n_neighbors=5, p=2, radius=1.0)
+print(colored('测试模型并做些推荐', 'red', attrs=['reverse', 'blink']))
+
+query_index = np.random.choice(us_canada_user_rating_pivot.shape[0])
+distances, indices = model_knn.kneighbors(us_canada_user_rating_pivot.iloc[query_index, :].reshape(1, -1), 
+    n_neighbors = 6)
+
+for i in range(0, len(distances.flatten())):
+    if i == 0:
+        print('Recommendations for {0}:\n'.format(us_canada_user_rating_pivot.index[query_index]))
+    else:
+        print('{0}: {1}, with distance of {2}:'.format(i,
+            us_canada_user_rating_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
