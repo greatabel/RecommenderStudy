726651856a657bec50f466327f8b2d68f6470a53
greatabel <myreceiver2for2github@gmail.com>
2018-07-26 11:50:35 +0800

    sum_rating

--- abel_demo/i9non_personalised.py
+++ abel_demo/i9non_personalised.py
@@ -1,11 +1,21 @@
 import  pandas as pd
 import  numpy as np
 
+
+from termcolor import colored
+
 ratings = pd.read_csv("movies_ratings_tags_csv/ratings.csv",encoding="ISO-8859-1")
 movies = pd.read_csv("movies_ratings_tags_csv/movies.csv",encoding="ISO-8859-1")
 tags = pd.read_csv("movies_ratings_tags_csv/tags.csv",encoding="ISO-8859-1")
 
 
+print(colored('Damped means', 'green', attrs=['reverse', 'blink']))
+
 rating_mean = ratings.groupby(['movieId'])[['rating']].mean()\
                      .rename(columns={'rating': 'mean_rating'}).reset_index()
 print(rating_mean.head(10))
+
+ratings_sum = ratings.groupby(['movieId'])[['rating']].sum()\
+                     .rename(columns={'rating': 'sum_rating'}).reset_index()
+
+print(ratings_sum.head(10))
