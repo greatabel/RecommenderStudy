72bd7c7e0dff1ed13332e1d22207ac84d61b4851
greatabel <myreceiver2for2github@gmail.com>
2018-07-26 16:33:13 +0800

    damp mean

--- abel_demo/i9non_personalised.py
+++ abel_demo/i9non_personalised.py
@@ -17,5 +17,26 @@ print(rating_mean.head(10))
 
 ratings_sum = ratings.groupby(['movieId'])[['rating']].sum()\
                      .rename(columns={'rating': 'sum_rating'}).reset_index()
-
 print(ratings_sum.head(10))
+
+
+print(colored('Calculating damped mean using k = 5', 'green', attrs=['reverse', 'blink']))
+print("ratings['rating'].mean()=", ratings["rating"].mean())
+ratings_sum['sum_rating_factor']=ratings_sum['sum_rating'] + 5*(ratings["rating"].mean())
+print(ratings_sum.head(10))
+ratings_count = ratings.groupby(['movieId'])[['rating']].count()\
+                       .rename(columns = {'rating': 'count_rating'}).reset_index()
+ratings_count['count_rating_factor'] = ratings_count['count_rating'] + 5
+print(ratings_count.head(10))
+
+ratings_damped = pd.merge(ratings_sum,ratings_count[['movieId',
+            'count_rating','count_rating_factor']],on=['movieId'],how='left')
+
+print(colored('ratings_damped', 'green', attrs=['reverse', 'blink']))
+print(ratings_damped.head(10))
+ratings_damped['damped_mean'] = ratings_damped['sum_rating_factor']/ ratings_damped['count_rating_factor']
+
+ratings_mean_dampmean = pd.merge(rating_mean[['movieId','mean_rating']],
+    ratings_damped[['movieId','damped_mean']],on=['movieId'],how='left')
+ratings_mean_dampmean = ratings_mean_dampmean.sort_values(['mean_rating'], ascending=False)
+print(ratings_mean_dampmean.head(10))
