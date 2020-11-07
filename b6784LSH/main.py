4db30e2349d1e790f8a66cebbf0653917c35b733
greatabel <myreceiver2for2github@gmail.com>
2020-11-07 21:52:19 +0800

    load numpy

--- b6784LSH/main.py
+++ b6784LSH/main.py
@@ -1,13 +1,14 @@
+import numpy as np
 import argparse
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='proper argument to run experiment')
-    parser.add_argument('--d', type=str, default='/',
+    parser.add_argument('-d', type=str, default='/',
                         help="Data file path")
-    parser.add_argument('--s', type=int, default=2020,
+    parser.add_argument('-s', type=int, default=2020,
                         help='Random seed (by using np.random.seed(int)) ')
-    parser.add_argument('--m', type=str, default='js',
+    parser.add_argument('-m', type=str, default='js',
                         help='Similarity measure (js / cs / dcs)')
     args = parser.parse_args()
     return args
@@ -15,10 +16,12 @@ def parse_args():
 
 def main():
     args = parse_args()
-    print('是否使用GPU:', args.d, args.s, args.m)
+    print('args:', args.d, args.s, args.m)
+    data = np.load(args.d)
+    print(data, data.shape)
 
 
 
 if __name__ == "__main__":
     main()
-    # python3 main.py -d /very/long/path/to/data.npy -s 2020 -m js
+    # python3 main.py -d /Users/abel/Downloads/spare_time/b6784_900_recommand_doing/user_movie_rating.npy  -s 2020 -m js
